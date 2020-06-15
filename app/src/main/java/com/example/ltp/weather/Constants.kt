085c3a52e35c3727f0fe84387de0db0bc9b372b2
cyliong <liongtheprogrammer@gmail.com>
2020-06-15 20:53:10 +0800

    Extract constants

--- app/src/main/java/com/example/ltp/weather/Constants.kt
+++ app/src/main/java/com/example/ltp/weather/Constants.kt
@@ -3,3 +3,6 @@ package com.example.ltp.weather
 const val BASE_URL = "https://api.worldweatheronline.com/premium/v1/"
 
 const val API_KEY = ""
+
+const val GET_CITIES_REQUEST = "search.ashx?format=json&key=$API_KEY"
+const val GET_WEATHER_REQUEST = "weather.ashx?num_of_days=1&format=json&key=$API_KEY"
--- app/src/main/java/com/example/ltp/weather/network/WeatherApiService.kt
+++ app/src/main/java/com/example/ltp/weather/network/WeatherApiService.kt
@@ -1,6 +1,7 @@
 package com.example.ltp.weather.network
 
-import com.example.ltp.weather.API_KEY
+import com.example.ltp.weather.GET_CITIES_REQUEST
+import com.example.ltp.weather.GET_WEATHER_REQUEST
 import com.example.ltp.weather.model.City
 import com.example.ltp.weather.model.Weather
 import retrofit2.Call
@@ -9,10 +10,10 @@ import retrofit2.http.Query
 
 interface WeatherApiService {
 
-    @GET("search.ashx?format=json&key=$API_KEY")
+    @GET(GET_CITIES_REQUEST)
     fun getCities(@Query("query") name: String): Call<List<City>>
 
-    @GET("weather.ashx?num_of_days=1&format=json&key=$API_KEY")
+    @GET(GET_WEATHER_REQUEST)
     fun getWeather(@Query("q") cityName: String): Call<Weather>
 
 }
