fe36dbdf329a5b4714a19b8b7ab2f28a64e6a063
cyliong <liongtheprogrammer@gmail.com>
2020-06-24 21:50:29 +0800

    Simplify coroutine calls with ViewModelScope

--- app/src/main/java/com/example/ltp/weather/city/CityViewModel.kt
+++ app/src/main/java/com/example/ltp/weather/city/CityViewModel.kt
@@ -5,10 +5,13 @@ import android.graphics.BitmapFactory
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ltp.weather.model.Weather
 import com.example.ltp.weather.repository.WeatherRepository
 import com.example.ltp.weather.storage.HistoryManager
-import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.net.URL
 import javax.inject.Inject
 
@@ -20,30 +23,18 @@ class CityViewModel(private val state: SavedStateHandle) : ViewModel() {
     @Inject
     lateinit var weatherRepository: WeatherRepository
 
-    private val completableJob = Job()
-    private val coroutineScope = CoroutineScope(Dispatchers.Main + completableJob)
-
     val weatherData = MutableLiveData<Weather>()
     val weatherIconData = MutableLiveData<Bitmap>()
 
-    fun loadWeather(cityName: String) {
-        coroutineScope.launch(Dispatchers.Main) {
-            weatherData.value = getWeather(cityName)
+    fun loadWeather(cityName: String) = viewModelScope.launch {
+        weatherData.value = getWeather(cityName)
 
-            // Add city name to the recent list
-            historyManager.addCityNameWithCountry(cityName)
-        }
+        // Add city name to the recent list
+        historyManager.addCityNameWithCountry(cityName)
     }
 
-    fun loadWeatherImage(imageUrl: String) {
-        coroutineScope.launch(Dispatchers.Main) {
-            weatherIconData.value = getBitmapAsync(imageUrl)
-        }
-    }
-
-    override fun onCleared() {
-        coroutineScope.cancel()
-        super.onCleared()
+    fun loadWeatherImage(imageUrl: String) = viewModelScope.launch {
+        weatherIconData.value = getBitmapAsync(imageUrl)
     }
 
     private suspend fun getWeather(cityName: String) =
--- app/src/main/java/com/example/ltp/weather/home/HomeViewModel.kt
+++ app/src/main/java/com/example/ltp/weather/home/HomeViewModel.kt
@@ -3,10 +3,13 @@ package com.example.ltp.weather.home
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ltp.weather.model.City
 import com.example.ltp.weather.repository.WeatherRepository
 import com.example.ltp.weather.storage.HistoryManager
-import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class HomeViewModel(private val state: SavedStateHandle) : ViewModel() {
@@ -17,23 +20,13 @@ class HomeViewModel(private val state: SavedStateHandle) : ViewModel() {
     @Inject
     lateinit var weatherRepository: WeatherRepository
 
-    private val completableJob = Job()
-    private val coroutineScope = CoroutineScope(Dispatchers.Main + completableJob)
-
     val cityData = MutableLiveData<List<City>>()
 
-    fun loadCities(name: String) {
-        coroutineScope.launch(Dispatchers.Main) {
-            cityData.value = getCities(name)
-        }
+    fun loadCities(name: String) = viewModelScope.launch {
+        cityData.value = getCities(name)
     }
 
     fun getRecentCities() = historyManager.getRecentCities()
-
-    override fun onCleared() {
-        coroutineScope.cancel()
-        super.onCleared()
-    }
 
     private suspend fun getCities(name: String) =
         withContext(Dispatchers.IO) {
