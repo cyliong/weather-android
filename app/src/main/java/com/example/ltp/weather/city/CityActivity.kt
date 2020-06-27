adab6c77ea285ba1e43763356b0f14124b67c9fc
cyliong <liongtheprogrammer@gmail.com>
2020-06-27 12:39:56 +0800

    Simplify data loading with liveData builder function

--- app/src/main/java/com/example/ltp/weather/city/CityActivity.kt
+++ app/src/main/java/com/example/ltp/weather/city/CityActivity.kt
@@ -26,20 +26,15 @@ class CityActivity : AppCompatActivity() {
         val cityName = intent.getStringExtra(EXTRA_CITY) ?: ""
         supportActionBar?.title = cityName
 
-        viewModel.loadWeather(cityName)
-
-        viewModel.weatherData.observe(this, Observer {
-            val imageUrl = it.iconUrl
-            viewModel.loadWeatherImage(imageUrl)
-
+        viewModel.getWeather(cityName).observe(this, Observer { weather ->
             binding.contentCity.apply {
-                textViewTemperature.text = it.temperature.toString() + "°c"
-                textViewDescription.text = it.description
-                textViewHumidity.text = it.humidity.toInt().toString() + "%"
+                textViewTemperature.text = weather.temperature.toString() + "°c"
+                textViewDescription.text = weather.description
+                textViewHumidity.text = weather.humidity.toInt().toString() + "%"
             }
-        })
-        viewModel.weatherIconData.observe(this, Observer {
-            binding.contentCity.imageViewWeather.setImageBitmap(it)
+            viewModel.getWeatherImage(weather.iconUrl).observe(this, Observer { bitmap ->
+                binding.contentCity.imageViewWeather.setImageBitmap(bitmap)
+            })
         })
     }
 
--- app/src/main/java/com/example/ltp/weather/city/CityViewModel.kt
+++ app/src/main/java/com/example/ltp/weather/city/CityViewModel.kt
@@ -1,17 +1,12 @@
 package com.example.ltp.weather.city
 
-import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.example.ltp.weather.model.Weather
+import androidx.lifecycle.liveData
 import com.example.ltp.weather.repository.WeatherRepository
 import com.example.ltp.weather.storage.HistoryManager
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import java.net.URL
 import javax.inject.Inject
 
@@ -23,25 +18,20 @@ class CityViewModel(private val state: SavedStateHandle) : ViewModel() {
     @Inject
     lateinit var weatherRepository: WeatherRepository
 
-    val weatherData = MutableLiveData<Weather>()
-    val weatherIconData = MutableLiveData<Bitmap>()
-
-    fun loadWeather(cityName: String) = viewModelScope.launch {
-        weatherData.value = weatherRepository.getWeather(cityName)
+    fun getWeather(cityName: String) = liveData {
+        val weather = weatherRepository.getWeather(cityName)
 
         // Add city name to the recent list
         historyManager.addCityNameWithCountry(cityName)
+
+        emit(weather)
     }
 
-    fun loadWeatherImage(imageUrl: String) = viewModelScope.launch {
-        weatherIconData.value = getBitmapAsync(imageUrl)
-    }
-
-    private suspend fun getBitmapAsync(url: String): Bitmap =
-        withContext(Dispatchers.IO) {
-            URL(url).openStream().use {
-                return@withContext BitmapFactory.decodeStream(it)
-            }
+    fun getWeatherImage(imageUrl: String) = liveData(Dispatchers.IO) {
+        val bitmap = URL(imageUrl).openStream().use {
+            BitmapFactory.decodeStream(it)
         }
+        emit(bitmap)
+    }
 
 }
