8ce511f57112eb4a06c433248986b1e6ddf931cf
cyliong <liongtheprogrammer@gmail.com>
2021-06-19 21:53:01 +0800

    Annotate the Application class

--- app/src/main/java/com/example/ltp/weather/WeatherApplication.kt
+++ app/src/main/java/com/example/ltp/weather/WeatherApplication.kt
@@ -3,7 +3,9 @@ package com.example.ltp.weather
 import android.app.Application
 import com.example.ltp.weather.di.AppComponent
 import com.example.ltp.weather.di.DaggerAppComponent
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 open class WeatherApplication : Application() {
 
     val appComponent: AppComponent by lazy {
