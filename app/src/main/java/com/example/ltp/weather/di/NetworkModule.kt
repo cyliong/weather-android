f8ab8b8b72d630ae34c4b263771f0171fa21befe
cyliong <liongtheprogrammer@gmail.com>
2021-06-20 11:31:32 +0800

    Annotate modules

--- app/src/main/java/com/example/ltp/weather/di/NetworkModule.kt
+++ app/src/main/java/com/example/ltp/weather/di/NetworkModule.kt
@@ -3,7 +3,10 @@ package com.example.ltp.weather.di
 import com.example.ltp.weather.BASE_URL
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 
+@InstallIn(SingletonComponent::class)
 @Module
 class NetworkModule {
 
--- app/src/main/java/com/example/ltp/weather/di/StorageModule.kt
+++ app/src/main/java/com/example/ltp/weather/di/StorageModule.kt
@@ -4,7 +4,10 @@ import com.example.ltp.weather.storage.SharedPreferencesStorage
 import com.example.ltp.weather.storage.Storage
 import dagger.Binds
 import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 
+@InstallIn(SingletonComponent::class)
 @Module
 abstract class StorageModule {
 
