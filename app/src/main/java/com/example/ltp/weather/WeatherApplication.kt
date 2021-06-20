f49267f597ddcb1b094ec5046aa0353a1c20ba6e
cyliong <liongtheprogrammer@gmail.com>
2021-06-20 21:28:50 +0800

    Refactor the Application class

--- app/src/main/java/com/example/ltp/weather/WeatherApplication.kt
+++ app/src/main/java/com/example/ltp/weather/WeatherApplication.kt
@@ -1,15 +1,7 @@
 package com.example.ltp.weather
 
 import android.app.Application
-import com.example.ltp.weather.di.AppComponent
-import com.example.ltp.weather.di.DaggerAppComponent
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-open class WeatherApplication : Application() {
-
-    val appComponent: AppComponent by lazy {
-        DaggerAppComponent.factory().create(applicationContext)
-    }
-
-}
+open class WeatherApplication : Application()
