22e02244c3a388818536de6f7b8d30e083804315
cyliong <liongtheprogrammer@gmail.com>
2021-06-29 21:20:06 +0800

    Refactor WeatherRepositoryTest

--- app/src/test/java/com/example/ltp/weather/repository/WeatherRepositoryTest.kt
+++ app/src/test/java/com/example/ltp/weather/repository/WeatherRepositoryTest.kt
@@ -1,18 +1,36 @@
 package com.example.ltp.weather.repository
 
-import com.example.ltp.weather.BASE_URL
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.ltp.weather.network.WeatherApiService
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.HiltTestApplication
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+import javax.inject.Inject
 
+@HiltAndroidTest
+@RunWith(AndroidJUnit4::class)
+@Config(application = HiltTestApplication::class)
 class WeatherRepositoryTest {
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
 
     private lateinit var weatherRepository: WeatherRepository
 
+    @Inject
+    lateinit var weatherApiService: WeatherApiService
+
     @Before
     fun setup() {
-        weatherRepository = WeatherRepository(BASE_URL)
+        hiltRule.inject()
+        weatherRepository = WeatherRepository(weatherApiService)
     }
 
     @Test
